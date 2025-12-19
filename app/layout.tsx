75269d9cde9885e3a5dfc939871bed860cd746af
Yuki Iwata <yukiiwata@iwtq2024.local>
2025-12-20 02:43:25 +0900

    chore: Change site title to NOMI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: "NOMI",
+  description: "あなたのためのフリマアプリ NOMI",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
-        <div className="bg-gray-100 min-h-screen">
-          <div className="max-w-lg mx-auto bg-white min-h-screen shadow-xl relative">
-            {children}
-            <Navbar />
-          </div>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
